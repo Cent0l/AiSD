b92cd66e209c6dbfdce11a29ec1523111e6c4911
Mateusz <115219630+Cent0l@users.noreply.github.com>
2023-10-25 10:59:42 +0200

    Update and rename ch*jwie.cs to hdysfa.cs

--- hdysfa.cs
+++ hdysfa.cs
@@ -9,12 +9,17 @@ namespace WinFormsApp1
 
         private void btn_1_Click(object sender, EventArgs e)
         {
-            String str1 = "5 4 3 7 2";
-            int[] tab = fromstrign(str1);
-            tb_2.Text = str1;
+            String strdef = tb_input.Text;
+            int[] tab = fromstring(strdef);
             int[] ret = Selectsort(tab);
-            String str2 = tostring(ret);
-            tb_1.Text = str2;
+            int[] ret2 = Bubblesort(tab);
+            int[] ret3 = Insertsort(tab);
+            String str1 = tostring(ret);
+            String str2 = tostring(ret2);
+            String str3 = tostring(ret3);
+            lbl_1.Text = str1;
+            lbl_2.Text = str2;
+            lbl_3.Text = str3;
         }
         String tostring(int[] tab)
         {
@@ -25,9 +30,9 @@ namespace WinFormsApp1
             }
             return ret;
         }
-        int[] fromstrign(String napis)
+        int[] fromstring(String napis)
         {
-            var liczbyS = napis.Trim().Split(' ');
+            var liczbyS = napis.Trim().Split(' ');//var domysla sie ze to tablica stringow a to wynika z prawej strony
             int[] liczby=new int[liczbyS.Length];
             for(int i=0;i<liczbyS.Length; i++)
             {
@@ -35,7 +40,7 @@ namespace WinFormsApp1
             }
             return liczby;
         }
-        int[] Bubblesortint(int[] tab)
+        int[] Bubblesort(int[] tab)
         {
             int temp = tab[1];
             int change = 1;
@@ -81,7 +86,41 @@ namespace WinFormsApp1
             return tab;
         }
 
+        int[] Insertsort(int[] tab)
+        {
+            int min = tab[0];
+            int temp = 0;
+            int ind = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                min = tab[i];
+                ind = i;
+                for (int j = i; j < tab.Length; j++)
+                {
+                    if (tab[j] < min)
+                    {
+                        temp = tab[i];
+                        tab[i] = min;
+                        tab[ind] = temp;
+
+                    }
+
+                }
+                
+            }
+            return tab;
+        }
         private void tb_1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_1_Click(object sender, EventArgs e)
         {
 
         }
