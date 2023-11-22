113daebcb3b98f7272ff16450d1589f2a3bfa115
Mateusz <115219630+Cent0l@users.noreply.github.com>
2023-11-22 11:06:25 +0100

    Update wezelki.cs

--- wezelki.cs
+++ wezelki.cs
@@ -1,9 +1,12 @@
-namespace WinFormsApp1
+using System.IO.Pipes;
+using System.Security;
+using System.Xml.Linq;
+
+namespace WinFormsApp2
 {
     public partial class Form1 : Form
     {
-        String napis = "";
-
+        string napis = "";
         public Form1()
         {
             InitializeComponent();
@@ -16,42 +19,104 @@ namespace WinFormsApp1
 
         private void btn_1_Click(object sender, EventArgs e)
         {
-            var w1 = new Wezel(5);//dodawanie wezla
-            var w2 = new Wezel(3);
-            var w3 = new Wezel(1);
-            var w4 = new Wezel(2);
-            var w5 = new Wezel(6);
-            var w6 = new Wezel(4);
-            w1.dzieci.Add(w2);
-            w1.dzieci.Add(w3);
-            w1.dzieci.Add(w4);
-            w2.dzieci.Add(w5);
-            w2.dzieci.Add(w6);
-            A(w1);
-            MessageBox.Show(napis);
-           
+
+            var w1 = new Wezel(1);//dodawanie wezla
+            var w2 = new Wezel(2);
+            var w3 = new Wezel(3);
+            var w4 = new Wezel(4);
+            var w5 = new Wezel(5);
+            var w6 = new Wezel(6);
+            w1.Add(w2);
+            w1.Add(w3);
+            w2.Add(w4);
+            w2.Add(w5);
+            w4.Add(w6);
+            w3.Add(w6);
+            
+           B(w1);
+           MessageBox.Show(napis);
+
+           napis = "";
+           odwiedzone.Clear();
+
 
         }
         void A(Wezel w)
         {
-            napis += " " + w.wartosc.ToString();
-            for (int i=0;i<w.dzieci.Count;i++)
+            MessageBox.Show(w.wartosc.ToString());
+            for (int i = 0; i < w.sasiedzi.Count; i++)
             {
-                A(w.dzieci[i]);
-
+                A(w.sasiedzi[i]);
             }
-            
+
+        }
+        List<Wezel> odwiedzone = new();
+        void B(Wezel w)
+        { 
+            odwiedzone.Add(w);
+            napis += w.wartosc.ToString();
+            foreach (var sasiedzi in w.sasiedzi)
+            {
+                if(!odwiedzone.Contains(sasiedzi))
+                    B(sasiedzi);
+            }
         }
     }
-    
-    public class Wezel
+    public class Wezell
     {
 
         public int wartosc;
-        public List<Wezel> dzieci = new List<Wezel>();
-        public Wezel(int n)
+        public List<Wezell> dzieci = new List<Wezell>();
+        public Wezell(int n)
         {
             this.wartosc = n;
         }
     }
+
+
+    public class Wezel
+    {
+
+        public int wartosc;
+        public List<Wezel> sasiedzi = new List<Wezel>();
+        public Wezel(int n)
+        {
+            this.wartosc = n;
+        }
+        public void Add(Wezel w)
+        {
+            if (w == this)
+                return;
+            if (!this.sasiedzi.Contains(w))
+                this.sasiedzi.Add(w);
+            if (!w.sasiedzi.Contains(this))
+                w.sasiedzi.Add(this);
+
+
+        }
+    }    
+        public class Wezel3
+    {
+
+            public int wartosc;
+            public List<Wezel3> rodzic = new();
+            public List<Wezel3> lewe = new();
+            public List<Wezel3> prawe = new();
+            public Wezel3(int n)
+            {
+            this.wartosc = n;
+            }
+            public void Addl(Wezel3 w)
+        {
+            if(w==this)
+            {
+                return;
+            }
+
+        }
+
+
+
+    }
 }
+
