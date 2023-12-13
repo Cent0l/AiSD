986edfdf8d275b22b9f4ba8367281ade0c0ffd95
Mateusz <115219630+Cent0l@users.noreply.github.com>
2023-12-13 10:29:17 +0100

    Create temp.cs

---
+++ temp.cs
@@ -0,0 +1,20 @@
+public class Wezel4
+{
+    int wartosc;
+    public Wezel4(int wartosc)
+    {
+        this.wartosc = wartosc;
+    }
+}
+public class Krawedz
+{
+    int waga;
+    public Wezel4 poczatek;
+    public Wezel4 koniec;
+}
+public class graf
+{
+ List<Wezel4>wezly=new List<Wezel4>();
+ List<Wezel4>krawedz=new List<Wezel4>();
+
+}
